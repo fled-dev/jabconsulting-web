5c7ad6e6dd519bd253b9c0c940bfc828a934ba9b
fled-dev <fled-dev@users.noreply.github.com>
2025-11-18 10:02:26 +0000

    deploy: f830aba726e1b64c19062197dec6b038cc35d4e3

---
+++ _next/static/chunks/pages/_app-65d133386903175b.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[636],{6528:(_,n,p)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return p(2790)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[593,792],()=>(n(6528),n(6048))),_N_E=_.O()}]);
